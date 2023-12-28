7eafa0a11463473a4a5d3d3f17275923386ef482
FlyingPig69 <124304224+FlyingPig69@users.noreply.github.com>
2023-12-28 17:32:31 +0000

    Update main.py

--- api/main.py
+++ api/main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 #todo Add unconfirmed
 
 # Define the base URL
-base_url= 'http://213.239.193.208:9053'
+base_url= 'http://128.253.41.49:9053'
 flask_url = 'https://ergo-node-explorer.vercel.app'
 
 # Define specific paths
