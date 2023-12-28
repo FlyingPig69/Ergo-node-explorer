deda73335ec9ce93c91191039ecc2374c5743c2f
FlyingPig69 <124304224+FlyingPig69@users.noreply.github.com>
2023-12-28 15:32:41 +0000

    TX history by address and token rich lists

--- api/main.py
+++ api/main.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 import requests
+from collections import defaultdict
 from datetime import datetime
 import json
 
 app = Flask(__name__)
 
+#todo Add unconfirmed
+
 # Define the base URL
-base_url = 'http://213.239.193.208:9053'
-flask_url = 'https://ergo-node-explorer.vercel.app/'
+base_url = 'http://192.168.0.20:9051'
+#base_url= 'http://213.239.193.208:9053'
+flask_url = 'http://192.168.0.20:5000'
 
 # Define specific paths
 transaction_path = '/blockchain/transaction/'
 transaction_u_path= '/transactions/unconfirmed/byTransactionId/'
 token_path = '/blockchain/token/'
+token_u_path ='/blockchain/box/unspent/byTokenId/'
 address_path = '/blockchain/balance'
+info_path = '/info'
 
 box_path = '/blockchain/box/'
 indexed_path='/blockchain/indexedHeight'
@@ -41,12 +47,13 @@ def process_transaction(transaction_id): #process transaction and split into inp
 
     if transaction_response.status_code == 200:
         transaction_data = transaction_response.json()
-
+        token_ids_to_check = [] #initialize tokens to check for delta
         def process_assets(assets):
             asset_details = []
             for asset in assets:
                 token_name, decimals,description = get_token_name(asset['tokenId'])
                 amount = asset.get('amount')
+                token_ids_to_check.append(asset['tokenId'])
                 asset_details.append({
                     'token_id': asset['tokenId'],
                     'token_name': token_name,
@@ -71,12 +78,26 @@ def process_transaction(transaction_id): #process transaction and split into inp
         input_details = process_input_output_data('inputs', 'inputs')
         output_details = process_input_output_data('outputs', 'outputs')
 
+        delta_result = calculate_delta(input_details, output_details)
+
+#----get delta for ERG and Tokens per address
+#        for address, deltas in delta_result.items():
+#            print(f"Address: {address}")
+#            for key, value in deltas.items():
+#                if key == 'value':
+#                    print(f"  Delta Value: {value}")
+#                else:
+#                    token_id, token_name, decimals = key
+#                    print(f"  Token ID: {token_id}, Token Name: {token_name}, Decimals: {decimals}, Delta Amount: {value}")
+#----
+
         return {
             'Transaction ID': transaction_id,
             'inclusionHeight': transaction_data.get('inclusionHeight', 'inclusionHeight'),
             'creationHeight': transaction_data["outputs"][0]["creationHeight"],
             'Input Details': input_details,
             'Output Details': output_details,
+            'delta': delta_result,
             'tx_json': transaction_data
         }
     else:
@@ -248,31 +269,155 @@ def search_transaction():
             except TypeError as e:
                 error_message = "An error occurred: {}".format(e)
                 return render_template('error_template.html', error_message=error_message)
+        elif search_type == 'token_id':
+            try:
+                return redirect(url_for('token_details', tokenid=search_text))
+            except TypeError as e:
+                error_message = "An error occurred: {}".format(e)
+                return render_template('error_template.html', error_message=error_message)
 
 
 @app.route('/address/<address>')
 def address_details(address):
-    print("it went here")
 
-    transactions = requests.post(base_url+transaction_path+"byAddress?offset=0&limit=5", headers=headers, data=address)
+    offset = request.args.get('offset', default=0, type=int)
+
+    transactions = requests.post(base_url+transaction_path+f"byAddress?offset={offset}&limit=10", headers=headers, data=address)
     transactions = transactions.json()
     tx_ids = [item["id"] for item in transactions["items"]]
-    print("Tx:", tx_ids)
+    #print("Tx:", tx_ids)
 
     transaction_details = []
 
     for i in tx_ids:
         process_tx = process_transaction(i);
         transaction_details.append(process_tx)
-    print(transaction_details)
+
     data = requests.post(base_url+address_path, headers=headers, data=address)
     address_data=data.json()
 
+    #get current block height
+    info_url = get_url(f"{info_path}")
+    full_height = requests.get(info_url)
+    if full_height.status_code == 200:
+        full_height = full_height.json()
+
+    full_height = full_height['fullHeight']
+    #print("Height:",full_height)
     if address_data:
 
-        return render_template('address.html', address=address, address_data=address_data, transaction_details=transaction_details,flask_url=flask_url,)
+        return render_template('address.html', full_height=full_height, address=address, address_data=address_data, transaction_details=transaction_details,flask_url=flask_url, offset=offset)
+    else:
+        return "Failed to retrieve transaction details."
+
+#this calculated the delta for a transaction
+def calculate_delta(input_details, output_details):
+    # Create a defaultdict to store delta values by address
+    delta_by_address = defaultdict(dict)
+
+    # Combine input and output details into a single list
+    all_details = input_details + output_details
+
+    # Get unique addresses from both input and output details
+    unique_addresses = set(detail['address'] for detail in all_details)
+
+    for address in unique_addresses:
+        # Get details for the address from both input and output
+        address_details = [detail for detail in all_details if detail['address'] == address]
+
+        # Sum up the input and output values for the address
+        input_value = sum(detail['value'] for detail in address_details if detail in input_details)
+        output_value = sum(detail['value'] for detail in address_details if detail in output_details)
+
+        # Calculate the delta for the value (output minus input)
+        delta_value = output_value - input_value
+
+        # Store the delta for the value in the defaultdict
+        delta_by_address[address]['value'] = delta_value
+
+        # Create a defaultdict to store token amounts for the address
+        token_amounts = defaultdict(float)
+
+        # Iterate through details for the address
+        for detail in address_details:
+            assets = detail.get('assets', [])
+            for asset in assets:
+                token_id = asset['token_id']
+                token_name = asset['token_name']
+                decimals = asset['decimals']
+                amount = asset['amount']
+
+                # Subtract the amounts for each token in input
+                if detail in input_details:
+                    token_amounts[(token_id, token_name, decimals)] -= amount
+                # Add the amounts for each token in output
+                elif detail in output_details:
+                    token_amounts[(token_id, token_name, decimals)] += amount
+
+        # Iterate through unique token details
+        for token_details, amount in token_amounts.items():
+            token_id, token_name, decimals = token_details
+
+            # Adjust the amount based on the decimal value
+            adjusted_amount = amount / 10 ** decimals
+
+            # Exclude entries with an adjusted amount of 0
+            if adjusted_amount != 0:
+                # Store the delta in the defaultdict
+                delta_by_address[address][(token_id, token_name, decimals)] = adjusted_amount
+
+    return dict(delta_by_address)
+
+
+@app.route('/token/<tokenid>')
+def token_details(tokenid):
+    offset = 0
+    limit = 100
+    tokens_data = []  # Initialize an empty list to store the results
+
+    while True:
+        token_url = get_url(f"{token_u_path}{tokenid}?offset={offset}&limit={limit}")
+        tokens = requests.get(token_url)
+        tokens_json = tokens.json()
+
+        if tokens.status_code == 200:
+            data = tokens_json
+
+            # Assuming data is a list of dictionaries
+            for item in data:
+                # Access values in each dictionary
+                value_of_key = item.get('key')  # Replace 'key' with the actual key in your data
+                #print(value_of_key)
+                # Append the entire dictionary if needed
+                tokens_data.append(item)
+
+            # Check if there are more items
+            if len(data) < limit:
+                break  # Break the loop if there are no more items
+
+            offset += limit  # Increment the offset for the next iteration
+        else:
+            print(f"Error: {tokens.status_code}")
+            break  # Break the loop in case of an error
+
+    name, decimals, description = get_token_name(tokenid)
+    decimals = 10 ** decimals
+    grouped_data = defaultdict(int)
+
+    for entry in tokens_data:
+        for asset in entry.get("assets", []):
+            if asset.get("tokenId") == tokenid:
+                grouped_data[entry["address"]] += asset["amount"]/decimals
+
+    # Sort the data by highest to lowest amount
+    tokens_data = sorted(grouped_data.items(), key=lambda x: x[1], reverse=True)
+    holders = len(tokens_data)
+    # Print the sorted result
+
+    if tokens:
+        return render_template('token.html', tokens_data=tokens_data, name=name, decimals=decimals, description=description, holders=holders, flask_url=flask_url)
     else:
         return "Failed to retrieve transaction details."
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=False, threaded=True)
+    app.run(host="0.0.0.0", debug=True, threaded=True)
